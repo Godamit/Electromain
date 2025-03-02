0775ebf1b8c8d2f2777e956e6e11574bd2cc7547
amit <shivabhai461@gmail.com>
2025-03-02 15:43:32 +0530

    starrt2

--- js/electro.js
+++ js/electro.js
@@ -39,7 +39,7 @@ navMenu.addEventListener('click', () => {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 20, 2025 15:37:25").getTime();
+var countDownDate = new Date("Mar 27, 2025 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
